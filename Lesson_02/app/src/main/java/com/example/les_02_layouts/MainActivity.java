63483a223792c6850a352d8ca7df31ad595210db
Andrei Lyubishkin <vk-andrei@mail.ru>
2022-06-22 19:09:27 +0300

    Les_02. Done

--- Lesson_02/app/src/main/java/com/example/les_02_layouts/MainActivity.java
+++ Lesson_02/app/src/main/java/com/example/les_02_layouts/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint2_example);
+        setContentView(R.layout.activity_main);
     }
 }
